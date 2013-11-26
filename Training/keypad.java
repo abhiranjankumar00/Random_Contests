ef0ce7e12760568ae9bdd1a98248570bada73cba
Abhiranjan Kumar <abhiranjan.kumar00@gmail.com>
2013-11-26 20:02:44 +0530

    Added backtrack

--- Training/keypad.java
+++ Training/keypad.java
@@ -24,81 +24,15 @@ class Main
     }
   }
 
-	public static void main(String [] args) throws Exception
-	{
-		initShinChanNohara();
-
-    String keys[] = {"abc", "def", "ghi"};
-    int N = keys.length;
-    char cur[] = new char[N];
-
+  static void solve(String []keys) {
+    char cur[] = new char[keys.length];
     backtrack(0, cur, keys);
+  }
 
-	}
-
-//================================================================
-	static BufferedReader br;//{{{
-	static StringTokenizer strTok;
-
-	static void initShinChanNohara() {
-		br = new BufferedReader(new InputStreamReader(System.in));
-		strTok = null;
-	}
-
-	static void initShinChanNohara(String file) {
-		try {
-			br = new BufferedReader(new FileReader(file));
-			strTok = null;
-		}
-		catch(FileNotFoundException e) {
-			writeln(e);
-		}
-	}
-
-	static String getLine() {
-		try {
-			String st = br.readLine();
-			return st;
-		}
-		catch(IOException e) {
-			return null;
-		}
-
-	}
-
-	static String getToken() {
-		while(strTok == null || strTok.hasMoreTokens() == false) {
-			String st = getLine();
-			if(st == null)
-				return null;
-			strTok = new StringTokenizer(st);
-		}
-		return strTok.nextToken();
-	}
-
-	static int getInt() {
-		return Integer.parseInt(getToken()); 
-	}
-	static double getDouble() {
-		return Double.parseDouble(getToken());
-	}
-	static long getLong() {
-		return Long.parseLong(getToken());
-	}
-	
-	static void write(Object ... ob) {
-		for(int i = 0; i < ob.length; i++) {
-			if(i > 0)
-				System.out.print(" ");
-			System.out.print(ob[i]);
-		}
-	}
-	static void writeln(Object ... ob) {
-		write(ob);
-		System.out.println();
-	}
-	static void debug(Object ... ob) {
-		System.out.println(Arrays.deepToString(ob));
-	}//}}}
+	public static void main(String [] args) throws Exception
+  {
+    String keys[] = {"abc", "def", "ghi"};
+    solve(keys);
+  }
 }
 
